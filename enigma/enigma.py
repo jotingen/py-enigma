b4bebeb3e750870c0c46e13b2dabd5ddda877224
James Tingen <jotingen@gmail.com>
2021-04-25 04:03:49 -0400

    Add enigma machine class. Capture/print keyboard input.

--- enigma/enigma.py
+++ enigma/enigma.py
@@ -6,10 +6,27 @@ Type in encoded, get out message
 
 """
 
+import readchar # type: ignore
+
+from .enigma_machine import Enigma
+
 
 def main() -> None:
     """ main """
-    pass
+
+    e = Enigma()
+
+    while True:
+        char = readchar.readchar()
+        # Handle traps
+        if char in ("\x03", "\x04", "\x1a"):
+            return
+        # Handle newline
+        elif char in ("\x0a", "\x0d"):
+            print(char, flush=True)
+        # Encode
+        else:
+            print(e.encode(char), end="", flush=True)
 
 
 if __name__ == "__main__":
---
+++ enigma/enigma_machine.py
@@ -0,0 +1,18 @@
+"""
+Enigma Machine 
+"""
+
+from typing import Optional
+
+
+class Enigma:
+    """ Enigma """
+
+    def __init__(
+        self,
+    ) -> None:
+        pass
+
+    def encode(self, c: str) -> Optional[str]:
+        """ encode """
+        return c
