6751f8ba6b239eab5af9fdc4b4937a389ef6d86b
James Tingen <jotingen@gmail.com>
2021-04-24 21:54:16 -0400

    Initial

---
+++ enigma/enigma.py
@@ -0,0 +1,16 @@
+"""
+Enigma emulator
+
+Type in message, get out encoded
+Type in encoded, get out message
+
+"""
+
+
+def main() -> None:
+    """ main """
+    pass
+
+
+if __name__ == "__main__":
+    main()
